c4ac6ccaa5746b1882fa1dd81087f49bf2200c8a
Łukasz Pająk <l.pajak@nomtek.com>
2021-08-26 08:29:29 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/nomtek/quizler_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nomtek.quizler_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/question.dart
@@ -0,0 +1,9 @@
+class Question {
+  late String question;
+  late bool answer;
+
+  Question({String q: '', bool a: true}) {
+    question = q;
+    answer = a;
+  }
+}
